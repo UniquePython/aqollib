4c828e4066457c5ad6ab82b9f28ad6dd4eb9b03b
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-03-04 17:11:54 +0530

    Add more dtypes

--- include/dtype/dtype.h
+++ include/dtype/dtype.h
@@ -4,9 +4,17 @@
 typedef enum e_dtype
 {
     INT,
+    LONG,
+    LONG_LONG,
+    SHORT,
+    UNSIGNED_INT,
+    UNSIGNED_CHAR,
+    SIZE_T,
     DOUBLE,
     FLOAT,
     CHAR
 } DType;
+
+size_t GetTypeSize(DType dtype);
 
 #endif
--- src/dtype/dtype.c
+++ src/dtype/dtype.c
@@ -10,6 +10,24 @@ size_t GetTypeSize(DType dtype)
     case INT:
         type_size = sizeof(int);
         break;
+    case LONG:
+        type_size = sizeof(long);
+        break;
+    case LONG_LONG:
+        type_size = sizeof(long long);
+        break;
+    case SHORT:
+        type_size = sizeof(short);
+        break;
+    case UNSIGNED_INT:
+        type_size = sizeof(unsigned int);
+        break;
+    case UNSIGNED_CHAR:
+        type_size = sizeof(unsigned char);
+        break;
+    case SIZE_T:
+        type_size = sizeof(size_t);
+        break;
     case DOUBLE:
         type_size = sizeof(double);
         break;
